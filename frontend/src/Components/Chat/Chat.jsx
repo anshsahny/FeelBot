898868e68f9671dc23534dbd5d52a58c0923742a
Ansh Sahny <anshsahny@gmail.com>
2023-05-08 20:49:48 -0400

    Connected socket to frontend

--- frontend/src/Components/Chat/Chat.jsx
+++ frontend/src/Components/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -7,12 +7,40 @@ import UserList from "../UserList/UserList";
 import Sentiment from "../Sentiment/Sentiment";
 import Chatbox from "../Chatbox/Chatbox";
 
+import io from 'socket.io-client'
+
 import './Chat.css'
+
+const ENDPOINT = 'http://localhost:8000/'
+console.log(ENDPOINT)
 
 const Chat = props => {
     const [signedIn, setSignedIn] = useState(false)
+    const [id, setId] = useState('')
     const [username, setUsername] = useState('')
     const [message, setMessage] = useState('')
+    const [users, setUsers] = useState('')
+
+    const socketRef = useRef()
+
+    useEffect(() => {
+        socketRef.current = io.connect(ENDPOINT, { transports: ['websocket'] })
+
+        socketRef.current.on('your id', id => {
+            setId(id)
+        })
+
+        socketRef.current.on('get users', users => {
+            console.log(users)
+            setUsers(users)
+        })
+    }, [])
+
+    useEffect(() => {
+        if (signedIn) {
+            socketRef.current.emit('new user', { Username: username })
+        }
+    }, [signedIn, username])
 
     const sendMessage = e => {
         console.log('send message')
@@ -28,7 +56,7 @@ const Chat = props => {
                         <h3>Welcome, {username}!</h3>
                         <Row md={2}>
                             <Col>
-                                <UserList />
+                                <UserList users={users} />
                             </Col>
                             <Col>
                                 <Sentiment />
@@ -36,7 +64,7 @@ const Chat = props => {
                         </Row>
                         <Row>
                             <Col>
-                                <Chatbox message={message} setMessage={setMessage} sendMessage={sendMessage} />
+                                <Chatbox id={id} message={message} setMessage={setMessage} sendMessage={sendMessage} />
                             </Col>
                         </Row>
                     </>
